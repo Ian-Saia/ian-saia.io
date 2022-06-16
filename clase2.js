f1a90098a5a6f01f1d581b89282d2da5a961358b
Ian-Saia <104043117+Ian-Saia@users.noreply.github.com>
2022-06-15 22:10:22 -0300

    Add files via upload

---
+++ clase2.js
@@ -0,0 +1,11 @@
+
+
+//for (let i=0 ; i <= 10 ; i++) {
+
+//console.log("el valor de i es", i)}
+
+for(let valor1= prompt('ingrese el valor de valor1'); valor1 <= 10 ; valor1++  ){
+
+    console.log(valor1)
+    
+    }
